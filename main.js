0cf909d1e08b5b0e7c4279cec6dd687f75b3ad0b
elsalye2-bee <elsalye2@gmail.com>
2026-05-28 01:53:34 +1000

    Use distinct chart types across visualisations

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ const kpiEmbedOpts = {
 };
 
 vegaEmbed("#obesity-map", "vega/obesity_choropleth.vg.json", embedOpts).catch(console.error);
-vegaEmbed("#interactive-health-map", "vega/interactive_health_map.vg.json", embedOpts).catch(console.error);
 vegaEmbed("#kpi-tiles", "vega/kpi_tiles.vg.json", kpiEmbedOpts).catch(console.error);
 vegaEmbed("#state-obesity-ranking", "vega/state_obesity_ranked_bar.vg.json", embedOpts).catch(console.error);
 vegaEmbed("#food-overview-detail", "vega/food_overview_detail.vg.json", embedOpts).catch(console.error);
@@ -21,5 +20,5 @@ vegaEmbed("#latest-food-category", "vega/latest_food_category_bar.vg.json", embe
 vegaEmbed("#obesity-age", "vega/obesity_age_annotated.vg.json", embedOpts).catch(console.error);
 vegaEmbed("#activity-obesity", "vega/activity_obesity_scatter.vg.json", embedOpts).catch(console.error);
 vegaEmbed("#activity-ranking", "vega/activity_ranked_bar.vg.json", embedOpts).catch(console.error);
-vegaEmbed("#gender-facets", "vega/gender_age_facets.vg.json", embedOpts).catch(console.error);
+vegaEmbed("#gender-dumbbell", "vega/gender_age_dumbbell.vg.json", embedOpts).catch(console.error);
 vegaEmbed("#food-stacked", "vega/food_category_stacked_area.vg.json", embedOpts).catch(console.error);
