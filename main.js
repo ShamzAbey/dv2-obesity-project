ca988424cd618d00c0748e9361b60f48072f52ae
sabe0016 <sabe0016@student.monash.edu>
2026-06-01 14:30:47 +1000

    Replace binned matrix with gender age line chart

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ const charts = [
 	["#gender-dumbbell", "vega/gender_age_dumbbell.vg.json", embedOpts],
 	["#gender-age-facets", "vega/gender_age_facets.vg.json", embedOpts],
 	["#gender-obesity-boxplot", "vega/gender_obesity_boxplot.vg.json", embedOpts],
-	["#activity-obesity-binned", "vega/activity_obesity_binned_matrix.vg.json", embedOpts],
+	["#gender-age-line", "vega/gender_age_line_chart.vg.json", embedOpts],
 	["#food-stacked", "vega/food_category_stacked_area.vg.json", embedOpts]
 ];
 
